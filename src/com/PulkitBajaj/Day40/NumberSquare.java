a5775c14929e985a048fe1e55826c85ca7331d67
Pulkit Bajaj <pulkitbajaj13@gmail.com>
2025-09-12 10:33:40 +0530

    Commit

---
+++ src/com/PulkitBajaj/Day40/NumberSquare.java
@@ -0,0 +1,13 @@
+package com.PulkitBajaj.Day40;
+
+public class NumberSquare {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            int num=1;
+            for(int j=1;j<=5;j++){
+                System.out.print(num++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
