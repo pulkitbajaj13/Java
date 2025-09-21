6579d331805f9f1f223d8b0c19c4605d1e4374ab
Pulkit Bajaj <pulkitbajaj13@gmail.com>
2025-09-21 15:05:48 +0530

    Commit

---
+++ src/com/PulkitBajaj/Day44/CountdownUsingRecursion.java
@@ -0,0 +1,19 @@
+package com.PulkitBajaj.Day44;
+
+import java.util.Scanner;
+
+public class CountdownUsingRecursion {
+    static void countdown(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        countdown(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        countdown(n);
+    }
+}
